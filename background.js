bfe5733367f685399536018497d347ca87f8ca18
Todd Hubbard <toddjhubbard93@gmail.com>
2023-01-12 15:49:13 -0700

    Modifying for manifest v3

--- background.js
+++ background.js
@@ -1,26 +1,13 @@
 'use strict';
 
-chrome.webRequest.onBeforeRequest.addListener(function (details) {
-	if (details.method !== 'GET') {
-		return;
-	}
-
-	var parts = details.url.split('?');
-	var query = queryString.parse(parts[1]);
-
-	query.share = 1;
-
-	return {
-		redirectUrl: parts[0] + '?' + queryString.stringify(query)
-	};
-}, {
-	urls: [
-		'*://quora.com/*',
-		'*://www.quora.com/*'
-	],
-	types: [
-		'main_frame'
-	]
-}, [
-	'blocking'
-]);
+chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
+	
+        if (details.url.indexOf("?share=1") != -1) { return; }
+		
+        let url = details.url + "?share=1";
+		
+        chrome.tabs.update(details.tabId, { url: url });
+		
+    },
+	{ url: [{ hostSuffix: 'quora.com' }] }
+);
